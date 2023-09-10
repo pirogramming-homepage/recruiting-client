a77d731405646cded626a264778f5780d5943d5c
ywonchae1 <ywonchae62@gmail.com>
2023-09-10 19:45:16 +0900

    #3 feat: 실패 경로 추가
    스타일링은 안 함

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { RecruitForm } from './components/RecruitForm';
 import { SuccessPage } from './components/SuccessPage';
+import { FailPage } from './components/FailPage';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<RecruitForm />} />
         <Route path="/success" element={<SuccessPage />} />
+        <Route path="/fail" element={<FailPage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/FailPage.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const FailPage = (props) => {
+    return (
+        <div>뭔가 잘못되었습니다..</div>
+    )
+}
