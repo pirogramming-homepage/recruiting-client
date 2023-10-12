027438f1c632976fd514061b726d6935cc2e1110
Ubuntu <ubuntu@ip-172-31-6-75.ap-northeast-2.compute.internal>
2023-10-12 13:38:07 +0000

    #9 fix: 서버 주소 변경

--- src/components/Variables.js
+++ src/components/Variables.js
@@ -7,7 +7,7 @@ export const COLORS = {
 
 export const SERVER_URL =
 	process.env.NODE_ENV === 'production'
-	? 'http://43.201.16.162:3333/api'
+	? 'http://52.79.205.140:3333/api'
 	: 'http://localhost:3333/api';
 
 /* 서류 변수 수정 */
