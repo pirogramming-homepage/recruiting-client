0da3f8dbb20ad22bf0f4d6b0c3aa18dac1c54a53
ywonchae1 <ywonchae62@gmail.com>
2023-11-16 18:11:15 +0900

    #32 feat: 날짜 확인 함수 추가

--- src/components/utils.js
+++ src/components/utils.js
@@ -1,4 +1,4 @@
-import { SERVER_URL } from "./Variables";
+import { RECRUIT_END_DAY, RECRUIT_END_MONTH, RECRUIT_START_DAY, RECRUIT_START_MONTH, RECRUIT_YEAR, SERVER_URL } from './Variables'
 
 export const authoHyphen = (target) => {
 	return target.value
@@ -31,4 +31,21 @@ export const fetchGetApi = async (url) => {
 	if (res.ok) {
 		return res;
 	}
+}
+
+export default function dateCheck() {
+  // 접속 시간 리크루팅이 종료되었다면
+  const now = new Date()
+  const recruitStartDate = new Date(RECRUIT_YEAR, RECRUIT_START_MONTH - 1, RECRUIT_START_DAY)
+  const recruitEndDate = new Date(RECRUIT_YEAR, RECRUIT_END_MONTH - 1, RECRUIT_END_DAY)
+	// console.log('now', now)
+	// console.log('recruitStartDate', recruitStartDate)
+	// console.log('recruitEndDate', recruitEndDate)
+  if (now < recruitStartDate) {
+    return 'before' // 리크루팅 시작 전
+  }
+  else if (now > recruitEndDate) {
+    return 'after' // 리크루팅 종료 후
+  }
+	return 'ok'
 }
