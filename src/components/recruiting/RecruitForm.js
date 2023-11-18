4f9db7175b43c3c1f993d024eccbc5d8d941a1c8
ywonchae1 <ywonchae62@gmail.com>
2023-11-19 00:21:55 +0900

    #38 fix: 홈페이지 자동 저장 구현 실패..

--- src/components/recruiting/RecruitForm.js
+++ src/components/recruiting/RecruitForm.js
@@ -143,8 +143,6 @@ export const RecruitForm = (props) => {
             piro_level: LEVEL,
         }
         
-        // 홈페이지에 정보 저장
-        // const add_result = await addApplicantToHome(name, phone, level, major)
         // 코딩테스트 파일 저장
         const formData = new FormData()
         formData.append('coding-test', fileInfo)
@@ -162,20 +160,10 @@ export const RecruitForm = (props) => {
         console.log('file_res_json', file_res_json.status)
         console.log('data_res_json', data_response.status)
         console.log('email_res_json', email_response.status)
-        // console.log('add_result', add_result.status)
 
         if(file_res_json.status == true && data_response.status == 'success' && email_response.status == true) return 'success'
         else if(file_res_json.status == true && data_response.status == 'success' && email_response.status == false) return 'email false'
         else return 'fail'
-        /*
-        if(add_result.status == 'saved') {
-            if(file_res_json.status == true && data_response.status == 'success' && email_response.status == true) return 'success'
-            else if(file_res_json.status == true && data_response.status == 'success' && email_response.status == false) return 'email false'
-            else return 'fail'
-        } else {
-            return 'fail'
-        }
-        */
     }
 
     const handleSubmit = async (e) => {
